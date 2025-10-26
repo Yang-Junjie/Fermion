6659f17f16ab0cd732287063e5362aa6112827f0
Yang-Junjie <1973690778@qq.com>
2025-10-26 16:07:49 +0800

    feat(Renderer): 实现 OpenGL 缓冲区与顶点数组对象的封装
    新增了 OpenGL 平台下的 VertexBuffer、IndexBuffer 和 VertexArray 的具体实现，
    包括对应的创建逻辑和绑定、数据设置等接口。同时更新了 CMakeLists.txt 文件以包含新源文件。

---
+++ Fermion/Sources/Renderer/Buffer.cpp
@@ -0,0 +1,50 @@
+﻿#include "fmpch.hpp"
+#include "Renderer/Buffer.hpp"
+
+#include "Renderer/RendererAPI.hpp"
+
+#include "OpenGLBuffer.hpp"
+
+namespace Fermion
+{
+
+    std::shared_ptr<VertexBuffer> VertexBuffer::create(uint32_t size)
+    {
+        switch (RendererAPI::getAPI())
+        {
+        case RendererAPI::API::None:
+            return nullptr;
+        case RendererAPI::API::OpenGL:
+            return std::make_shared<OpenGLVertexBuffer>(size);
+        }
+
+        return nullptr;
+    }
+
+    std::shared_ptr<VertexBuffer> VertexBuffer::create(float *vertices, uint32_t size)
+    {
+        switch (RendererAPI::getAPI())
+        {
+        case RendererAPI::API::None:
+            return nullptr;
+        case RendererAPI::API::OpenGL:
+            return std::make_shared<OpenGLVertexBuffer>(vertices, size);
+        }
+
+        return nullptr;
+    }
+
+    std::shared_ptr<IndexBuffer> IndexBuffer::create(uint32_t *indices, uint32_t size)
+    {
+        switch (RendererAPI::getAPI())
+        {
+        case RendererAPI::API::None:
+            return nullptr;
+        case RendererAPI::API::OpenGL:
+            return std::make_shared<OpenGLIndexBuffer>(indices, size);
+        }
+
+        return nullptr;
+    }
+
+}
---
+++ Fermion/Sources/Renderer/VertexArray.cpp
@@ -0,0 +1,19 @@
+﻿#include "fmpch.hpp"
+#include "Renderer/VertexArray.hpp"
+
+#include "Renderer/RendererAPI.hpp"
+#include "OpenGLVertexArray.hpp"
+
+namespace Fermion {
+
+	std::shared_ptr<VertexArray> VertexArray::create()
+	{
+		switch (RendererAPI::getAPI())
+		{
+			case RendererAPI::API::None:    return nullptr;
+			case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLVertexArray>();
+		}
+		return nullptr;
+	}
+
+}
