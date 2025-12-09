0b1578498bbb17338d5f9c9d25d10e198c0d6e47
Yang-Junjie <1973690778@qq.com>
2025-12-09 16:01:11 +0800

    fix(opengl): 修复纹理数据对齐问题
    - 在纹理上传前设置像素对齐为1字节
    - 上传完成后恢复原始对齐设置
    - 确保纹理数据正确加载避免渲染异常

--- Fermion/Platform/OpenGL/OpenGLTexture.cpp
+++ Fermion/Platform/OpenGL/OpenGLTexture.cpp
@@ -110,7 +110,19 @@ namespace Fermion
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_rendererID);
 		glTextureStorage2D(m_rendererID, levels, m_internalFormat, m_width, m_height);
 
-		glTextureSubImage2D(m_rendererID, 0, 0, 0, m_width, m_height, m_dataFormat, GL_UNSIGNED_BYTE, data);
+		GLint previousAlignment;
+		glGetIntegerv(GL_UNPACK_ALIGNMENT, &previousAlignment);
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+		glTextureSubImage2D(
+			m_rendererID, 0,
+			0, 0,
+			m_width, m_height,
+			m_dataFormat,
+			GL_UNSIGNED_BYTE,
+			data);
+
+		glPixelStorei(GL_UNPACK_ALIGNMENT, previousAlignment);
 
 		glTextureParameteri(m_rendererID, GL_TEXTURE_MIN_FILTER, generateMipmap ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR);
 		glTextureParameteri(m_rendererID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -136,8 +148,22 @@ namespace Fermion
 
 		uint32_t bpp = m_dataFormat == GL_RGBA ? 4 : 3;
 		FERMION_ASSERT(size == m_width * m_height * bpp, "Data must be entire texture!");
-		glTextureSubImage2D(m_rendererID, 0, 0, 0, m_width, m_height, m_dataFormat, GL_UNSIGNED_BYTE, data);
+
+		GLint previousAlignment;
+		glGetIntegerv(GL_UNPACK_ALIGNMENT, &previousAlignment);
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+		glTextureSubImage2D(
+			m_rendererID, 0,
+			0, 0,
+			m_width, m_height,
+			m_dataFormat,
+			GL_UNSIGNED_BYTE,
+			data);
+
+		glPixelStorei(GL_UNPACK_ALIGNMENT, previousAlignment);
 	}
+
 	void OpenGLTexture2D::bind(uint32_t slot) const
 	{
 		FM_PROFILE_FUNCTION();
