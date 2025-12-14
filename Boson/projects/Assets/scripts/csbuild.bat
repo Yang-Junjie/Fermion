5284e80b66be8e286fab49e98b027e83f8027443
Yang-Junjie <1973690778@qq.com>
2025-12-14 21:41:13 +0800

    chore(build): 添加 C# 脚本编译批处理文件
    - 在 Photon 目录下创建 csbuild.bat 文件用于编译 Photon.dll
    - 在 Boson/projects/Assets/scripts 目录下创建 csbuild.bat 文件用于编译 Sandbox.dll
    - 配置编译参数包括引用 Photon.dll 和指定输出文件名
    - 添加编译所需的源代码文件列表到批处理脚本中
    - 更新 .gitignore 文件以跟踪 .idea 目录并忽略特定构建文件

---
+++ Boson/projects/Assets/scripts/csbuild.bat
@@ -0,0 +1,7 @@
+"C:\Program Files\Mono\bin\mcs.bat" -out:Sandbox.dll -target:library ^
+-r:Photon.dll ^
+../Boson/projects/Assets/scripts/CameraController.cs ^
+../Boson/projects/Assets/scripts/TestScript.cs ^
+../Boson/projects/Assets/scripts/PressureTest.cs
+
+
---
+++ Photon/csbuild.bat
@@ -0,0 +1,12 @@
+"C:\Program Files\Mono\bin\mcs.bat" -unsafe -out:Photon.dll -target:library ^
+../Photon/Vector3.cs ^
+../Photon/Scene/Entity.cs ^
+../Photon/Scene/Scene.cs ^
+../Photon/Renderer/DebugRenderer.cs ^
+../Photon/InternalCalls.cs ^
+../Photon/Scene/Components.cs ^
+../Photon/Vector2.cs ^
+../Photon/Vector4.cs ^
+../Photon/KeyCode.cs ^
+../Photon/Utils.cs ^
+../Photon/Input.cs
